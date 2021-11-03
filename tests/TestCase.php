7d4b182b4b3a00886d01c74038fb288e79b4e6f9
Clem Blanco <clemblanco@users.noreply.github.com>
2021-11-03 16:15:41 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,8 +16,7 @@ abstract class TestCase extends Orchestra
     /**
      * Get package providers.
      *
-     * @param \Illuminate\Foundation\Application $app
-     *
+     * @param  \Illuminate\Foundation\Application  $app
      * @return array
      */
     protected function getPackageProviders($app)
@@ -31,7 +30,7 @@ abstract class TestCase extends Orchestra
     /**
      * Define environment setup.
      *
-     * @param \Illuminate\Foundation\Application $app
+     * @param  \Illuminate\Foundation\Application  $app
      * @return void
      */
     protected function getEnvironmentSetUp($app)
